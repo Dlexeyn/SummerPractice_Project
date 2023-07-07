d4bcf2611db71907d31179739f147ff1b2951bd6
Dlexeyn <galenko.aleksej17@gmail.com>
2023-07-07 11:39:50 +0300

    Some refactoring, improve interface

--- AStar/src/Main.java
+++ AStar/src/Main.java
@@ -1,7 +1,12 @@
 import graphics.*;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         View view = new View();
+        SwingUtilities.invokeLater(() -> {
+            view.init();
+        });
     }
 }
